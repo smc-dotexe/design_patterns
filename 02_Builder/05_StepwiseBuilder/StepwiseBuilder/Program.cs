7ed30787d3996f7e8d95ff0a1039cfcd7de75dab
shanemcguire <shane.mcguire@vogappdevelopers.com>
2022-02-14 13:27:34 -0700

    stepwise/functional/faceted

---
+++ 02_Builder/05_StepwiseBuilder/StepwiseBuilder/Program.cs
@@ -0,0 +1,81 @@
+﻿// To use a stepwise builder, we have to construct it in a certain order.
+// First have to specify the CarType, then specify the WheelSize.
+// We are going to return one interface for configuring a CarType.
+// Then another interface for WheelSize.
+// Then a 3rd interface for building a car.
+public enum CarType
+{
+    Sedan, Crossover
+}
+
+public class Car
+{
+    public CarType Type;
+    public int WheelSize;
+}
+
+public interface ISpecifyCarType
+{
+    ISpecifyWheelSize OfType(CarType type);
+}
+
+public interface ISpecifyWheelSize 
+{
+    IBuildCar WithWheels(int size);
+}
+
+public interface IBuildCar 
+{
+    public Car Build();
+}
+
+public class CarBuilder
+{
+    private class Impl :
+        ISpecifyCarType,
+        ISpecifyWheelSize,
+        IBuildCar
+    {
+        private Car car = new Car();
+        public ISpecifyWheelSize OfType(CarType type)
+        {
+            car.Type = type;
+            return this; // returning type of ISpecifyWheelSize
+        }
+
+        public IBuildCar WithWheels(int size)
+        {
+            // validation
+            switch (car.Type)
+            {
+                case CarType.Crossover when size < 17 || size > 20:
+                case CarType.Sedan when size < 15 || size > 17:
+                    throw new ArgumentException($"Wrong size of wheel for {car.Type}");
+            }
+
+            car.WheelSize = size;
+            return this;
+        }
+        public Car Build()
+        {
+            return car;
+        }
+    }
+    public static ISpecifyCarType Create()
+    {
+        return new Impl();
+    }
+}
+
+public class StepwiseCarBuilder
+{
+    static void Main(string[] args)
+    {
+        var car = CarBuilder.Create()
+            .OfType(CarType.Crossover) // ISpecifyCarType
+            .WithWheels(19)            // ISpecifyWheelSize
+            .Build();                  // IBuildCar       
+    }
+}
+
+
---
+++ 02_Builder/06_FunctionalBuilder/FunctionalBuilder/Program.cs
@@ -0,0 +1,89 @@
+﻿public class Person
+{
+    public string Name, Position;
+
+}
+
+// Making it more reusable in the class below this
+//public sealed class PersonBuilder // sealed means you cannot inherit from it
+//{
+//    private readonly List<Func<Person, Person>> actions 
+//        = new List<Func<Person, Person>>();
+
+//    public PersonBuilder Called(string name)
+//        => Do(p => p.Name = name);
+
+//    public PersonBuilder Do(Action<Person> action)
+//        => AddAction(action);
+
+//    public Person Build()
+//        => actions.Aggregate(new Person(), (p, f) => f(p));
+
+//    private PersonBuilder AddAction(Action<Person> action)
+//    {
+//        actions.Add(p =>
+//        {
+//            action(p);
+//            return p;
+//        });
+
+//        return this;
+//    }
+//}
+
+public abstract class FunctionalBuilder<TSubject, TSelf>
+    where TSelf : FunctionalBuilder<TSubject, TSelf>
+    where TSubject : new () // need a default constructor at the very least
+{
+    private readonly List<Func<Person, Person>> actions
+        = new List<Func<Person, Person>>();
+
+    public TSelf Do(Action<Person> action)
+        => AddAction(action);
+
+    public Person Build()
+        => actions.Aggregate(new Person(), (p, f) => f(p));
+
+    private TSelf AddAction(Action<Person> action)
+    {
+        actions.Add(p =>
+        {
+            action(p);
+            return p;
+        });
+
+        return (TSelf)this;
+    }
+}
+
+public sealed class PersonBuilder
+    : FunctionalBuilder<Person, PersonBuilder>
+{
+    public PersonBuilder Called(string name)
+        => Do(p => p.Name = name);
+}
+
+
+
+
+
+public static class PersonBuilderExtensions
+{
+    public static PersonBuilder WorksAs
+        (this PersonBuilder builder, string position)
+        => builder.Do(p => p.Position = position);
+}
+
+
+static class FunctionalBuilderProgram
+{
+    public static void Main(string[] args)
+    {
+        var person = new PersonBuilder()
+            .Called("Shane")
+            .WorksAs("Developer")
+            .Build();
+
+        Console.WriteLine(person.Name);
+    }
+}
